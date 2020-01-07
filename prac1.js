acd957959bc36c5299b8f72d173c175d45828e71
aaronan-stockx <aaronan@stockx.com>
2020-01-07 05:06:10 -0500

    fleshed out prac1.js

--- prac1.js
+++ prac1.js
@@ -1,1 +1,10 @@
 //testing node
+//finding memory
+const os = require('os');
+
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
+
+
+console.log(`Total Memory ${totalMemory}`);
+console.log(`Total Memory ${freeMemory}`);
